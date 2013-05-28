618e532b48921cc1206658e6e56363429a5c3713
Logan Sears <lsears322@gmail.com>
2013-05-28 15:15:38 -0600

    Initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Retap::Application.config.secret_token = '128fce6da05c2c72eae49accfe55943f4228e687cea67f330e3fc3e249a25aea9ccefde6cab401eec7fbab4a07aa78f23f5ac6bbfbbece3293df176ea2bcf182'
